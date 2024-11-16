9418092d510bee0945edebf78de5df2a7da2ec90
Aristóteles Ferrão <afneto1960@gmail.com>
2024-11-16 16:03:11 -0300

    GameDTO e busca game por id

--- src/main/java/com/Sumauma/dslist/services/GameService.java
+++ src/main/java/com/Sumauma/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.Sumauma.dslist.dto.GameDTO;
 import com.Sumauma.dslist.dto.GameMinDTO;
 import com.Sumauma.dslist.entities.Game;
 import com.Sumauma.dslist.repositories.GameRepository;
@@ -15,12 +17,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<GameMinDTO> findAll() {
-		
-		List<Game> result = gameRepository.findAll();
-//		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-//		return dto;
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
-
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long Id) {
+		Game result = gameRepository.findById(Id).get();
+		return new GameDTO(result);
 	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
 }
