693310f0e50e8219c395e34a7331f7b143dcaaf8
Aristóteles Ferrão <afneto1960@gmail.com>
2024-11-17 16:41:34 -0300

    lists replacement

--- src/main/java/com/Sumauma/dslist/services/GameListService.java
+++ src/main/java/com/Sumauma/dslist/services/GameListService.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.Sumauma.dslist.dto.GameListDTO;
-import com.Sumauma.dslist.dto.GameMinDTO;
 import com.Sumauma.dslist.entities.GameList;
+import com.Sumauma.dslist.projections.GameMinProjection;
 import com.Sumauma.dslist.repositories.GameListRepository;
+import com.Sumauma.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -17,10 +18,30 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired 
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+				
 	}
 
 }
